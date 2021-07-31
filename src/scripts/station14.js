74a3d19c4643adb6f54bce2b1751ec21b99d46f2
daita2021 <daitamura2021@gmail.com>
2021-07-31 21:19:40 +0900

    full_Name => full_name

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -6,7 +6,7 @@ function getData() {
         { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
         { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
     ];
-    return test.map(buildFullName)
+    return test.map(buildFullName);
 }
 
 function buildFullName(data) {
@@ -14,8 +14,8 @@ function buildFullName(data) {
     // 氏名がわかるようにしつつ、半角スペースで繋いでください。
     // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
     const fullName = data.family_name + " " + data.first_name;
-    data.full_Name = fullName;
-    return data
+    data.full_name = fullName;
+    return data;
 }
 
 // 出力が想定される答え
